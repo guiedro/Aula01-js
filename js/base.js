930173166d4011a4098481922d6fa55aadab3f12
Pedro <guiedroliusti@gmail.com>
2025-02-10 10:40:28 -0300

    01- criando a página index.html e o arquivo inicial do JS

---
+++ js/base.js
@@ -0,0 +1,29 @@
+// Exibir o console
+// console só existe no navegador
+console.log('Olá, sou o console');
+console.log('Eu exibo conteúdo');
+
+// variaveis, recebe informações temporárias 
+let rmAluno = 666;
+let nomeAluno ='Rivaldo Jesus';
+let turmaAluno = '3TI';
+const ESCOLA_ALUNO = 'Lourdão';
+
+console.log(rmAluno, nomeAluno);
+console.log(turmaAluno, ESCOLA_ALUNO);
+
+console.log('Nome do aluno: ' + nomeAluno);
+console.log('Rm do aluno: ' + rmAluno);
+console.log('Turma do aluno: ' + turmaAluno);
+// template String
+console.log(`Nome do aluno ${nomeAluno} Rm do aluno ${rmAluno}`);
+
+turmaAluno = '3A';
+console.log(`Turma do aluno: ${turmaAluno}`);
+turmaAluno = '3C';
+console.log(`Turma do aluno: ${turmaAluno}`);
+
+
+
+
+
